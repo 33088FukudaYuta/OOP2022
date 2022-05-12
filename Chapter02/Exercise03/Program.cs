1c1f2ea78979f55695dfd49a0822baa7efba34ab
33088FukudaYuta <33088@ojs.ac.jp>
2022-05-12 15:14:58 +0900

    2.3解説前

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
-
+            int num;
+            Console.WriteLine("売上高の表示選択");
+            Console.WriteLine("1店舗別");
+            Console.WriteLine("2商品カテゴリー別");
+            Console.Write(">");
+            num = int.Parse(Console.ReadLine());
+            var sales = new SalesCounter("sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
+            if (num == 1) {
+                sales.GetPerStoreSales();
+            }
+            if(num == 2) {
+                sales.GetPerCategorySales();
+            }
         }
     }
 }
