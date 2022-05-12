2bd34bf6e1c9b4298a359fd42e620d0808beb73a
33088FukudaYuta <33088@ojs.ac.jp>
2022-05-12 14:03:16 +0900

    問題2.2完成

---
+++ Chapter02/Exercise02/InConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class InConverter {
+        private const double ratio = 0.0254;//定数rarioを定義
+
+        //メートルからフィートを求める(静的メソッド)
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //インチからメートルを求める(静的メソッド)
+        public static double ToMeter(int inti) {
+            return inti * ratio;
+        }
+    }
+}
---
+++ Chapter02/Exercise02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintInToMeterList(1, 10);
+            }
+            else {
+                PrintMeterToInList(1, 10);
+            }
+        }
+
+        //インチからメートルへの対応表を出力
+        private static void PrintInToMeterList(int start, int stop) {
+            for (int inti = start; inti <= stop; inti++) {
+                double meter = InConverter.ToMeter(inti);
+                Console.WriteLine("{0} in = {1:0.0000} m", inti, meter);
+            }
+        }
+
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToInList(int start, int stop) {
+            for (int meter = 1; meter <= 10; meter++) {
+                double inti = InConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} in", meter, inti);
+            }
+        }
+    }
+ }
+
