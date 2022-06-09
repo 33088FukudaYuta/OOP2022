b5ec0c94ac614389bc5b25579ca6fc3c00412c13
33088FukudaYuta <33088@ojs.ac.jp>
2022-06-09 14:11:04 +0900

    NumberGame完成

--- FormAppSample/NumberGame/Form1.cs
+++ FormAppSample/NumberGame/Form1.cs
@@ -11,8 +11,8 @@ using System.Windows.Forms;
 namespace NumberGame {
     public partial class Form1 : Form {
 
-        //乱数
-        private int randomNumber;
+        private int randomNumber;//乱数保存用
+        Random rand = new Random();//乱数オブジェクト生成
 
         public Form1() {
             InitializeComponent();
@@ -20,12 +20,30 @@ namespace NumberGame {
 
         private void button1_Click(object sender, EventArgs e) {
             //入力した値とあらかじめ取得した乱数を比較し結果を表示
-            
+            if(nudNum.Value == randomNumber) {
+                tbAns.Text = "正解";
+            }else if(nudNum.Value > randomNumber) {
+                tbAns.Text = "入力した値より低いです";
+            }else if(nudNum.Value < randomNumber) {
+                tbAns.Text = "入力した値より大きい値です";
+            }
         }
-
+        //アプリケーション起動時に一度だけ呼ばれるイベントハンドラ
         private void Form1_Load(object sender, EventArgs e) {
             //乱数取得
-            randomNumber = 2;
+            getRandom();
+        }
+
+        private void nudNum2_ValueChanged(object sender, EventArgs e) {
+            //randomNumber = rand.Next(1, maxValue: (int)nudNum2.Value);
+            //tbRand.Text = randomNumber.ToString();
+            getRandom();
+        }
+
+        //乱数取得
+        private void getRandom() {
+            randomNumber = rand.Next(1, maxValue: (int)nudNum2.Value);
+            tbRand.Text = randomNumber.ToString();
         }
     }
 }
