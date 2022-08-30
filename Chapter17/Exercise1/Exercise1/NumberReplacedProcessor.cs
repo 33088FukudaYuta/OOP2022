815a8b42c9715d46b13f9ec973444d86eb136e0e
33088FukudaYuta <33088@ojs.ac.jp>
2022-08-30 13:13:29 +0900

    演習問題17.1まで

---
+++ Chapter17/Exercise1/Exercise1/NumberReplacedProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace Exercise1 {
+    class NumberReplacedProcessor : TextProcessor{
+        private static Dictionary<char, char> dictionary = new Dictionary<char, char>() {
+            {'０','0' },{'１','1' },{'２','2' },{'３','3' },{'４','4' },
+            {'５','5' },{'６','6' },{'７','7' },{'８','8' },{'９','9' }
+        };
+
+        protected override void Execute(string line) {
+            String s = Regex.Replace(line, "[0-9]", p => ((char)(p.Value[0] - '０' + '0')).ToString());
+            Console.WriteLine(s);
+        }
+    }
+}
