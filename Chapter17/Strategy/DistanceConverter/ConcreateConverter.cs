a6f4f963d9f4df4d2270955e4482cc83f9f524b2
33088FukudaYuta <33088@ojs.ac.jp>
2022-09-02 14:04:16 +0900

    chapter17終了

--- Chapter17/Strategy/DistanceConverter/ConcreateConverter.cs
+++ Chapter17/Strategy/DistanceConverter/ConcreateConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DistanceConverter.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,7 +44,7 @@ namespace DistanceConverter {
     }
 
     public class MileConverter : ConverterBase {
-        protected override double Ratio => 1.609344;
+        protected override double Ratio => 1609.34;
         public override string UnitName => "マイル";
 
         public override bool IsMyUnit(string name) {
@@ -52,7 +53,7 @@ namespace DistanceConverter {
     }
 
     public class KmConverter : ConverterBase {
-        protected override double Ratio => 1;
+        protected override double Ratio => 1000;
         public override string UnitName => "キロメートル";
 
         public override bool IsMyUnit(string name) {
