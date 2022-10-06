250a9790eeffc120f3be9fb0623fc179caaeb38c
33088FukudaYuta <33088@ojs.ac.jp>
2022-10-06 15:38:07 +0900

    Exercise1_6終了

--- Chapter15/Exercise01/Program.cs
+++ Chapter15/Exercise01/Program.cs
@@ -72,7 +72,20 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_6() {
-
+            var groups = Library.Categories
+                .GroupJoin(Library.Books,
+                            c => c.Id,
+                            b => b.CategoryId,
+                            (c, books) => new {
+                                Category = c.Name,
+                                Books = books
+                            });
+            foreach (var group in groups.OrderBy(b => b.Category)) {
+                Console.WriteLine($"#{group.Category}");
+                foreach (var book in group.Books) {
+                    Console.WriteLine(book.Title);
+                }
+            }
         }
 
         private static void Exercise1_7() {
