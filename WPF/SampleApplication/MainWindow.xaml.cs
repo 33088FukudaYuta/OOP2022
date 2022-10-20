a024ec445c6d44e2db3de60e3631e50e04a9ceea
33088FukudaYuta <33088@ojs.ac.jp>
2022-10-20 15:54:37 +0900

    P036_SampleUnitConverter

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = GetSeason(); ;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
@@ -47,7 +48,45 @@ namespace SampleApplication {
         }
 
         private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            seasonTextBox.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private int GetSeason() {
+            var dt = DateTime.Now;
+            int ret = 0;
+            /*switch (dt.Month) {
+                case 3:
+                case 4:
+                case 5:
+                    ret = 0;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    ret = 1;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    ret = 2;
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    ret = 3;
+                    break;
+            }
+            return ret;*/
+
+            if (dt.Month == 9 || dt.Month == 10 || dt.Month == 11) {
+                return 2;
+            }else if(dt.Month == 12 || dt.Month == 1 || dt.Month == 2) {
+                return 3;
+            }else if(dt.Month == 3 || dt.Month == 4 || dt.Month == 5) {
+                return 0;
+            } else {
+                return 1;
+            }
         }
     }
 }
